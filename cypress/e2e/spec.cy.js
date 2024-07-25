9ff5bbbc8a2597ae7f9d6b4c2c8c5a6a77857507
restupambudi <restupambudi21195@proton.me>
2024-07-25 08:11:06 +0700

    Initial commit of project and Cypress tests

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,28 @@
+describe('test suite with web security disabled', () => { 
+  before(() => { 
+    // disable web security before the test suite Cypress.config(‘chromeWebSecurity’, false) 
+    Cypress.config('chromeWebSecurity', false)
+  })
+})
+
+describe('test suite with CSP enabled', () => { 
+  before(() => { 
+    // enable CSP before the test suite cy.config(‘modifyObstructiveCode’, false)
+    cy.config('modifyObstructiveCode', false) 
+  })
+})
+
+describe('template spec', () => {
+  it('passes', () => {
+    // cy.visit('http://ctas-qa.intranet.pajak.go.id', {failOnStatusCode: false})
+    cy.openWindow('http://ctas-qa.intranet.pajak.go.id')
+    Cypress.on('uncaught:exception', (err, runnable) => {
+      // returning false here prevents Cypress from
+      // failing the test
+      return false
+    })
+    cy.get('#username').type('admin')
+    cy.get('#password').type('Pajak123')
+    cy.get('.btn').click()
+  })
+})
